427d3869a46aa9ef6336e15de1500a8bf61755bb
Nicole Torres <nicolejoan9@yahoo.com>
2023-11-21 10:12:25 -0500

    changed pathing for thoughts

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,19 +2,19 @@ const router = require("express").Router();
 const { thoughtSchema } = require("../../models/thought");
 
 // get all thoughts
-router.get("/api/users/:userId/friends/:friendId", async (req, res) => {});
+router.get("/api/thoughts", async (req, res) => {});
 
 // get a single thought by id
-router.get("/api/users/:userId/friends/:friendId", async (req, res) => {});
+router.get("/api/thoughts", async (req, res) => {});
 
 // create a new thought  (don't forget to push the created thought's _id to the associated user's thoughts array field)
-router.post("/api/users/:userId/friends/:friendId", async (req, res) => {});
+router.post("/api/thoughts", async (req, res) => {});
 
 // update a thought by id
-router.put("/api/users/:userId/friends/:friendId", async (req, res) => {});
+router.put("/api/thoughts", async (req, res) => {});
 
 // remove thought by id
-router.delete("/api/users/:userId/friends/:friendId", async (req, res) => {});
+router.delete("/api/thoughts", async (req, res) => {});
 
 
 module.exports = router;
